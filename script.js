28261e653810ad533273f1da2256930255c1a485
Yaninna137 <rossioalvarez373@gmail.com>
2025-04-12 04:57:38 -0400

    Miniaplicación web separada en HTML, CSS y JS

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-function saludar() {
-    alert("¡Hola! Esta es una app conectada con GitHub 🧠");
+const numeroSecreto = Math.floor(Math.random() * 10) + 1;
+
+function verificarNumero() {
+  const entrada = document.getElementById("entrada").value;
+  const mensaje = document.getElementById("mensaje");
+
+  if (!entrada) {
+    mensaje.textContent = "Por favor ingresa un número.";
+    return;
   }
-  
+
+  const numeroUsuario = parseInt(entrada);
+
+  if (numeroUsuario === numeroSecreto) {
+    mensaje.textContent = "🎉 ¡Correcto! Adivinaste el número.";
+  } else if (numeroUsuario < numeroSecreto) {
+    mensaje.textContent = "📉 Muy bajo. Intenta con un número más alto.";
+  } else {
+    mensaje.textContent = "📈 Muy alto. Intenta con un número más bajo.";
+  }
+}
